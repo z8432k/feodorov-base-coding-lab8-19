51dd9d144e46ac05e87bc6adc83ef5957908800c
Alexander Feodorov <webmaster@unixcomp.org>
2020-01-15 11:55:47 +0300

    C++ done

--- Makefile
+++ Makefile
@@ -2,14 +2,14 @@ CXX = g++-9
 CC = gcc-9
 PROGRAM1 = files
 PROGRAM2 = cfiles
-CXXFLAGS = -Wall -Wextra -Wpedantic -ggdb -std=gnu++17 -O0 `pkg-config --cflags glib-2.0`
+CXXFLAGS = -Wall -Wextra -Wpedantic -ggdb -std=gnu++17 -O0
 CFLAGS = -Wall -Wextra -Wpedantic -ggdb -std=gnu11 -O0 `pkg-config --cflags glib-2.0`
 LDLIBS = `pkg-config --libs glib-2.0`
 
 default: bin/$(PROGRAM1) bin/$(PROGRAM2)
 
 bin/$(PROGRAM1): bin/$(PROGRAM1).o
-	$(CXX) $^ $(LDLIBS) -o $@
+	$(CXX) $^ -o $@
 
 bin/$(PROGRAM2): bin/$(PROGRAM2).o
 	$(CC) $^ $(LDLIBS) -o $@
--- src/files.cc
+++ src/files.cc
@@ -1,11 +1,94 @@
 #include <iostream>
+#include <string>
 #include <fstream>
-#include <cstdlib>
+#include <vector>
+#include <algorithm>
+
+static const char delims[] = ".!?;:";
 
 using namespace std;
 
+bool isDelimeter(char sym) {
+  size_t i = 0;
+  char delim;
+  while ((delim = delims[i++])) {
+    if (delim == sym) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool comparator(string &a, string &b) {
+  bool result = false;
+
+  if (a[1] == ' ' || a[1] == ',') {
+    result = true;
+  }
+
+  return result;
+}
+
 int main(void) {
-  cout << "= Files processing with fstream. =" << endl;
+  cout << "== Files read/write sample. ==" << endl << endl;
+
+  // FILE *in = fopen("data/sample", "r");
+  // FILE *out = fopen("data/out", "w");
+
+  ifstream in("data/sample");
+  ofstream out("data/out");
+
+  if (in.bad() || out.bad()) {
+    exit(EXIT_FAILURE);
+  }
+
+  vector<string> sentences;
+
+  char sym;
+  bool done = false;
+
+  do {
+    string str = "";
+    bool newStr = true;
+
+    while (true) {
+      in.get(sym);
+      if ((done = in.eof())) {
+        break;
+      }
+
+      if (newStr) {
+        if (sym == ' ' || sym == '\n') {
+          continue;
+        }
+
+        newStr = false;
+      }
+      else if (sym == '\n') {
+        continue;
+      }
+
+      str += sym;
+
+      if (isDelimeter(sym)) {
+        break;
+      }
+    }
+
+    sentences.push_back(str);
+
+  } while (!done);
+
+  sort(sentences.begin(), sentences.end(), comparator);
+
+  for (auto str: sentences) {
+    cout << str << endl << endl;
+    out << str << endl << endl;
+  }
+
+  in.close();
+  out.close();
 
   exit(EXIT_SUCCESS);
 }
